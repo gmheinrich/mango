b85c4b1c84c9c93bc7356c84dc26425262d7c2ef
Gabriel Heinrich <gmheinrich@hiberus.com>
2024-04-15 16:39:48 +0200

    Tailwind CSS added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+    title: 'Mango Range',
+    description: 'Mango FE Code Test'
+}
+
 export default function RootLayout({
     children,
   }: {
