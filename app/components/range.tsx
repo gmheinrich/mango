1cbd9274321f4dacc0ed9c91600f46709545d501
Gabriel Heinrich <gmheinrich@hiberus.com>
2024-04-17 13:57:37 +0200

    Range component updated

--- app/components/range.tsx
+++ app/components/range.tsx
@@ -1,42 +1,54 @@
 'use client';
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, ChangeEvent } from 'react';
 
-type Props = {
-  min?: number;
-  max?: number;
-  range?: number[];
-};
+interface RangeProps {
+  range: number[];
+}
 
-export const Range: React.FC<Props> = ({ min: initialMin, max: initialMax, range }) => {
-  const [minValue, setMinValue] = useState(initialMin || (range && range[0]) || 0);
-  const [maxValue, setMaxValue] = useState(initialMax || (range && range[range.length - 1]) || 100);
+interface MinMaxProps {
+  min: number;
+  max: number;
+}
+
+type Props = { rangeType: true } & RangeProps | { rangeType?: false } & MinMaxProps;
+
+export const Range: React.FC<Props> = ({ rangeType, ...props }) => { 
+
+  const { range } = props as RangeProps;
+  const { min, max } = props as MinMaxProps;
+  const [initialMin, setInitialMin] = useState<number>(min || 0);
+  const [initialMax, setInitialMax] = useState<number>(max || 0);
+  const [minValue, setMinValue] = useState<number>(min || (range && range[0]) || 0);
+  const [maxValue, setMaxValue] = useState<number>(max || (range && range[range.length - 1]) || 100);
   const lineRef = useRef<HTMLDivElement>(null);
   const isRange = useRef<boolean>(Array.isArray(range) && range.length > 0 && range.every(item => typeof item === 'number'))
 
   useEffect(() => {
     if (lineRef.current) {
       if (initialMin !== undefined && initialMax !== undefined) {
-        setMinValue(initialMin);
-        setMaxValue(initialMax);
-      } else if (isRange.current) {
-        setMinValue(range[0]);
-        setMaxValue(range[range.length - 1]);
-      }
+        setInitialMin(initialMin);
+        setInitialMax(initialMax);
+      } 
     }
-  }, [initialMin, initialMax, range]);
+  }, [initialMin, initialMax]);
 
-  const calcPercentage = (e: MouseEvent) => {
+  const calcPercentage = (e: MouseEvent): number => {
     const { left, width } = lineRef.current.getBoundingClientRect()
     return (e.clientX - left) / width
   }
 
+  const calcNewValue = (e: MouseEvent): number => {
+    let newValueIndex = Math.round(calcPercentage(e) * (range.length - 1))
+    newValueIndex = Math.max(0, Math.min(newValueIndex, range.length - 1))
+    return range[newValueIndex]
+  }
+
   const handleMinDrag = (e: MouseEvent) => {
+    e.preventDefault()
     if (lineRef.current) {
       if (isRange.current) {
-        let newValueIndex = Math.round(calcPercentage(e) * (range.length - 1))
-        newValueIndex = Math.max(0, Math.min(newValueIndex, range.length - 1))
-        const newValue = range[newValueIndex]
+        const newValue = calcNewValue(e)
         if (newValue < maxValue) {
           setMinValue(newValue)
         }
@@ -48,11 +60,10 @@ export const Range: React.FC<Props> = ({ min: initialMin, max: initialMax, range
   };
 
   const handleMaxDrag = (e: MouseEvent) => {
+    e.preventDefault()
     if (lineRef.current) {
       if (isRange.current) {
-        let newValueIndex = Math.round(calcPercentage(e) * (range.length - 1));
-        newValueIndex = Math.max(0, Math.min(newValueIndex, range.length - 1));
-        const newValue = range[newValueIndex];
+        const newValue = calcNewValue(e)
         if (newValue > minValue) {
           setMaxValue(newValue);
         }
@@ -82,20 +93,47 @@ export const Range: React.FC<Props> = ({ min: initialMin, max: initialMax, range
   const MinDragLeftPosition = isRange.current ? ((minValue - range[0]) / (range[range.length - 1] - range[0])) * 100 : ((minValue - initialMin) / (initialMax - initialMin)) * 100;
   const MaxDragLeftPosition = isRange.current ? ((maxValue - range[0]) / (range[range.length - 1] - range[0])) * 100 : ((maxValue - initialMin) / (initialMax - initialMin)) * 100;
   const SelectedRangeWidth = isRange.current ? ((maxValue - minValue) / (range[range.length - 1] - range[0])) * 100 : ((maxValue - minValue) / (initialMax - initialMin)) * 100;
+  
+  const handleOnChangeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value);
+    if (newValue >= 1 && newValue <= initialMax) {
+      setInitialMin(newValue);
+      setMinValue(newValue);
+    }
+  }; 
+
+  const handleOnChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value);
+    if (newValue >= initialMin) {
+      setInitialMax(newValue);
+      setMaxValue(newValue);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center w-full">
       <div className="flex items-center justify-between w-full">
-        <div className="mr-4">{minValue}</div>
-        <div ref={lineRef} className="relative w-full h-2 bg-gray-300 rounded-full">
+        <div className='relative w-[12%] mr-4'>
+          <input 
+            id="minValue" 
+            name="minValue" 
+            type="number" 
+            value={minValue} 
+            onChange={handleOnChangeMinValue} 
+            disabled={isRange.current}
+            className='peer w-full text-center focus:outline-2 focus:outline-black disabled:bg-inherit'
+          />
+          <span className='absolute w-4 right-3'>€</span>
+        </div>
+        <div ref={lineRef} className="relative w-[76%] h-2 bg-gray-300 rounded-full">
           <div
-            className="absolute -top-1/2 -ml-2 w-4 h-4 bg-black rounded-full shadow-md flex items-center justify-center cursor-grab"
-            style={{ left: `${MinDragLeftPosition}%` }}
+            className="absolute top-1/2 w-4 h-4 active:w-5 active:h-5 bg-black rounded-full shadow-md flex items-center justify-center cursor-grab"
+            style={{ left: `${MinDragLeftPosition}%`, transform: 'translate(-50%, -50%)' }}
             onMouseDown={handleMouseDownMin}
           ></div>
           <div
-            className="absolute -top-1/2 -ml-2 w-4 h-4 bg-black rounded-full shadow-md flex items-center justify-center cursor-grab"
-            style={{ left: `${MaxDragLeftPosition}%` }}
+            className="absolute top-1/2 w-4 h-4 active:w-5 active:h-5 bg-black rounded-full shadow-md flex items-center justify-center cursor-grab"
+            style={{ left: `${MaxDragLeftPosition}%`, transform: 'translate(-50%, -50%)' }}
             onMouseDown={handleMouseDownMax}
           ></div>
           <div
@@ -106,7 +144,18 @@ export const Range: React.FC<Props> = ({ min: initialMin, max: initialMax, range
             }}
           ></div>
         </div>
-        <div className='ml-4'>{maxValue}</div>
+        <div className='relative w-[12%] ml-4'>
+          <input 
+            id="maxValue" 
+            name="maxValue" 
+            type="number" 
+            value={maxValue} 
+            onChange={handleOnChangeMaxValue} 
+            disabled={isRange.current}
+            className="peer w-full text-center focus:outline-2 focus:outline-black disabled:bg-inherit"
+          />
+          <span className='absolute w-4 right-3'>€</span>
+        </div>
       </div>
     </div>
   );
--- app/exercise1/page.tsx
+++ app/exercise1/page.tsx
@@ -1,19 +1,12 @@
 import { Range } from "../components/range"
 
 export default function Page() {
-
-
-    return (
-        <>
-            <h1>Exercise 1</h1>
-            <div className="flex w-[800px] justify-center items-center p-10">
-              <Range min={5.99} max={70.99} />
-            </div>
-
-            <h1>Exercise 2</h1>
-            <div className="flex w-[800px] justify-center items-center p-10">
-              <Range range={[1.99, 5.99, 10.99, 30.99, 50.99, 70.99]} />
-            </div> 
-        </>
+  return (
+    <>
+      <h1>Exercise 1</h1>
+      <div className="flex w-[800px] justify-center items-center p-10">
+        <Range min={10.99} max={70.99} />
+      </div>
+    </>
     )
   }
